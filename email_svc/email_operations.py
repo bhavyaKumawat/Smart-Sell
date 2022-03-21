7a8821df5bee49d376f8a53abdf37be90fc388ba
bhavyaKumawat <bhavyakumawat99@gmail.com>
2022-03-21 20:42:55 +0530

    SMTP configuration

--- email_svc/email_operations.py
+++ email_svc/email_operations.py
@@ -6,20 +6,21 @@ from fastapi import BackgroundTasks, UploadFile
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 from pydantic import EmailStr
 
-
 logger = logging.getLogger()
 MAIL_FROM = os.environ["MAIL_FROM"]
 MAIL_PASSWORD = os.environ["MAIL_PASSWORD"]
+MAIL_SERVER = os.environ["MAIL_SERVER"]
+MAIL_USERNAME = os.environ["MAIL_USERNAME"]
 
 
 async def send(subject: str, body: str, background_tasks: BackgroundTasks, recipient: EmailStr, file):
     try:
         conf = ConnectionConfig(
             MAIL_FROM=MAIL_FROM,
-            MAIL_USERNAME=MAIL_FROM,
+            MAIL_USERNAME=MAIL_USERNAME,
             MAIL_PASSWORD=MAIL_PASSWORD,
             MAIL_PORT=587,
-            MAIL_SERVER="smtp.gmail.com",
+            MAIL_SERVER=MAIL_SERVER,
             MAIL_TLS=True,
             MAIL_SSL=False,
             USE_CREDENTIALS=True,
